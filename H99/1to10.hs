a2fdfe14af4cf92e0638865101ce0d5b107808af
Géraud <geraudster@gmail.com>
2011-06-24 22:19:18 +0200

    Problem 5 resolution

--- H99/1to10.hs
+++ H99/1to10.hs
@@ -18,4 +18,24 @@ elementAt' (x:_) 1 = x
 elementAt' (_:xs) n = elementAt' xs (n-1)
 elementAt' _ _ = error "Index out of bounds"
 
+ -- Problem 4
+myLength = length
 
+myLength' :: (Num n) => [a] -> n
+myLength' [] = 0
+myLength' (_:xs) = 1 + myLength' xs
+
+myLength'' xs = sum [1 | _ <- xs]
+
+ -- Problem 5
+myReverse = reverse
+
+myReverse' :: [a] -> [a]
+myReverse' (x:xs) = myReverse' xs ++ [x] 
+myReverse' _ = []
+
+myReverse'' :: [a] -> [a]
+myReverse'' list = reverse''' list []
+  where
+    reverse''' [] acc = acc
+    reverse''' (x:xs) acc = reverse''' xs (x:acc)
